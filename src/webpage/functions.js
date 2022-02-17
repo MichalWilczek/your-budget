123c156124d00d350f977c172ecd35209b42591f
MichalWilczek <mich.wilcz@gmail.com>
2022-02-18 00:07:36 +0100

    Add settings features.

--- src/webpage/functions.js
+++ src/webpage/functions.js
@@ -1,3 +1,10 @@
 function changePage() {
 	
 }
+
+function addSelectOption(selectID) {
+	var list = $("#selectList");
+	$.each(items, function(index, item) {
+	  list.append(new Option(item.text, item.value));
+	});
+}
