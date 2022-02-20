e5513f13c743a4ac5eb588668842741e5e5bc233
MichalWilczek <mich.wilcz@gmail.com>
2022-02-20 21:44:37 +0100

    Make current JS version object-oriented.

--- src/webpage/functions.js
+++ src/webpage/functions.js
@@ -1,29 +1,67 @@
 
-var paymentOptions = ["cash", "credit card", "debit card"];
-var incomeCategories = ["bank interest", "pay"]; 
-var expenseCategories = [
+class Options {
+	constructor(id, optionNames) {
+		this.options = [];
+		this.defaultOptions = ["other"]
+		this.id = id;
+		this.addOptions(optionNames);
+	}
+	addOption(optionName) {
+		if (!this.defaultOptions.includes(optionName) && optionName != this.id) {
+			this.options.push(optionName);
+			this.options.sort();
+		}
+	}
+	addOptions(optionNames) {
+		for (let optionName of optionNames) {
+			this.addOption(optionName);
+		}
+	}
+	removeOption(optionName) {
+		
+	}
+	changeOptionName(optionName, newOptionName) {
+		
+	}
+	showOptions(elementID) {
+		var optionsToShow = '<option value="'+ this.id +'" disabled selected>'+ this.id +'</option>';
+		for (let option of this.options) {
+			optionsToShow += '<option value="'+ option +'">'+ option +'</option>';
+		}
+		for (let option of this.defaultOptions) {
+			optionsToShow += '<option value="'+ option +'">'+ option +'</option>';
+		}
+		document.getElementById(elementID).innerHTML = optionsToShow;
+	}
+}
+
+function runAddIncome() {
+	incomeOptions.showOptions("income-category-select");
+}
+
+function runAddExpense() {
+	paymentOptions.showOptions("payment-option-select");
+	expenseOptions.showOptions("expense-category-select")
+}
+
+
+/*
+MAIN SCRIPT
+*/
+
+// DEFINITION OF VARIABLES
+let incomeOptions = new Options("category", ["bank interest", "pay"]);
+let paymentOptions = new Options(
+	"payment option",
+	["cash", "credit card", "debit card"]
+);
+let expenseOptions = new Options(
+	"category", 
+	[
 	"appartment", "books", "children", "clothes", 
 	"debts", "donation", "entertainment", "food", 
 	"healthcare", "internet", "phone", "savings", 
 	"training", "transport", "travel", "tv", 
 	"retirement"
-]; 
-var otherElement = "other";
-
-function showSections(mainLabel, options, elementID) {
-	var section = '<option value="'+ mainLabel +'" disabled selected>'+ mainLabel +'</option>';
-	for (i=0; i<options.length; i++) {
-		section += '<option value="'+ options[i] +'">'+ options[i] +'</option>';
-	}
-	section += '<option value="other">other</option>';
-	document.getElementById(elementID).innerHTML = section;
-}
-
-function runAddIncome() {
-	showSections("category", incomeCategories, "income-category-select");
-}
-
-function runAddExpense() {
-	showSections("payment option", paymentOptions, "payment-option-select");
-	showSections("category", expenseCategories, "expense-category-select");
-}
+	]
+);
