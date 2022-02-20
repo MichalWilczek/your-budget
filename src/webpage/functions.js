5f93435f4039069ba38a1c5873b56661521c5cf0
MichalWilczek <mich.wilcz@gmail.com>
2022-02-20 18:52:38 +0100

    Move section logic to JS.

--- src/webpage/functions.js
+++ src/webpage/functions.js
@@ -1,10 +1,29 @@
-function changePage() {
-	
+
+var paymentOptions = ["cash", "credit card", "debit card"];
+var incomeCategories = ["bank interest", "pay"]; 
+var expenseCategories = [
+	"appartment", "books", "children", "clothes", 
+	"debts", "donation", "entertainment", "food", 
+	"healthcare", "internet", "phone", "savings", 
+	"training", "transport", "travel", "tv", 
+	"retirement"
+]; 
+var otherElement = "other";
+
+function showSections(mainLabel, options, elementID) {
+	var section = '<option value="'+ mainLabel +'" disabled selected>'+ mainLabel +'</option>';
+	for (i=0; i<options.length; i++) {
+		section += '<option value="'+ options[i] +'">'+ options[i] +'</option>';
+	}
+	section += '<option value="other">other</option>';
+	document.getElementById(elementID).innerHTML = section;
 }
 
-function addSelectOption(selectID) {
-	var list = $("#selectList");
-	$.each(items, function(index, item) {
-	  list.append(new Option(item.text, item.value));
-	});
+function runAddIncome() {
+	showSections("category", incomeCategories, "income-category-select");
+}
+
+function runAddExpense() {
+	showSections("payment option", paymentOptions, "payment-option-select");
+	showSections("category", expenseCategories, "expense-category-select");
 }
